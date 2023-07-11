560e68c61099a074e66f8ce20c4791a1d7770095
Joseph R Miles <joe@jrm.dev>
2023-07-11 09:39:14 -0700

    feat: added joke and loader markup

--- index.tsx
+++ index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { createRoot } from 'react-dom'
 
-interface Joke {
+interface TJoke {
   setup: string
   punchline: string
 }
 
-const fetchRandomJoke = async (): Promise<Joke> => {
+const fetchRandomJoke = async (): Promise<TJoke> => {
   const response = await fetch('https://official-joke-api.appspot.com/random_joke')
   const { setup, punchline } = await response.json()
 
@@ -16,7 +16,22 @@ const fetchRandomJoke = async (): Promise<Joke> => {
   }
 }
 
-const App = () => (<h1>Hello, World!</h1>)
+const Joke = ({ setup, punchline }: TJoke) => (
+  <>
+    <p>{setup}</p>
+    <p>{punchline}</p>
+  </>
+)
+
+const App = () => {
+  const [ joke, setJoke ] = React.useState<TJoke | null>(null)
+
+  React.useEffect(() => {
+    fetchRandomJoke().then((joke) => setJoke(joke))
+  }, [])
+
+  return joke ? <Joke {...joke} /> : <p>Loading your joke...</p>
+}
 
 const root = createRoot(document.getElementById("app-root"))
 root.render(<App />)
